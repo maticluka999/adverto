4f01f0b90c748de653866bea66d3db33eb45210a
Luka Matic <101013503+maticluka999@users.noreply.github.com>
2023-08-30 00:17:41 +0200

    fix navigation after ad update

--- adverto/web/src/pages/UpdateAdPage.tsx
+++ adverto/web/src/pages/UpdateAdPage.tsx
@@ -1,4 +1,4 @@
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import AdForm from '../components/ads/AdForm';
 import { Ad } from '../types';
 
@@ -10,9 +10,11 @@ function UpdateAdPage() {
   const location = useLocation();
   const { ad } = location.state as State;
 
+  const navigate = useNavigate();
+
   return (
     <div className='flex justify-center pt-10'>
-      <AdForm onCreateUpdateAd={(ad) => alert('ad updated')} ad={ad} />
+      <AdForm onCreateUpdateAd={(ad) => navigate('/')} ad={ad} />
     </div>
   );
 }
